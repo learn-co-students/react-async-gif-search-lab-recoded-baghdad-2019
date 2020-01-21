a335ab5608ecfa0bac031073b00cfc071b07ee7d
Sura-93 <suraaljailawi@gmailcom>
2020-01-21 16:42:11 +0300

    D

---
+++ src/components/GifList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class GifList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.gify.map(item => {
+          return (
+            <li>
+              <img src={item.images.original.url} alt="" />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
